10d3858850f781eab5c72d0ea6c3ab36e3df1cc0
ethan <izawaryosuke0101@gmail.com>
2021-07-20 15:04:34 +0900

    results.jsx作成

---
+++ src/components/Results.jsx
@@ -0,0 +1,13 @@
+const Results = () => {
+    return(
+        <>
+        <p>日付：</p>
+        <p>新規感染者：</p>
+        <p>感染者数：</p>
+        <p>新規回復者：</p>
+        <p>回復者総数：</p>
+
+        </>
+    )
+}
+export default Results;
