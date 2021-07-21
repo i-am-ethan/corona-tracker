4b9be9a18c585a587df8c2b500572982fe20d299
ethan <izawaryosuke0101@gmail.com>
2021-07-21 15:55:15 +0900

    header作成と適用

--- src/pages/WorldPage.jsx
+++ src/pages/WorldPage.jsx
@@ -1,11 +1,15 @@
+import Header from "../components/Header"
 import Title from "../components/Title";
 import Card from "../components/Card"
 
 const WorldPage = (props) => {
     return(
         <>
-        <Title />
-        <Card allCountriesData={props.allCountriesData} />
+        <div className="world-page-container">
+            <Header />
+            <Title />
+            <Card allCountriesData={props.allCountriesData} />
+        </div>
         </>
     )
 }
